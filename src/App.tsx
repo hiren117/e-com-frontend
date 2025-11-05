1bde6e61e7935f1f1b6c7e7bd03b6bd96321c6aa
Hiren Solanki <hirensolanki9697@gmail.com>
2025-11-05 12:58:40 +0530

    setup: added absolute import alias (@) and cleaned folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import "./App.css";                     // optional
-import { Navbar, Footer } from "./components/common";
-import { Hero, Features, Popular } from "./components/homePage";
+import { Navbar, Footer } from "@/components/common";
+import { Hero, Features, Popular } from "@/components/homePage";
 
 export default function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // 👇 lets you write "@/..." and have it map to "<project>/src"
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
